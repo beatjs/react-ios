7537b821d40c22871ed2de19aa57908e7b093bd9
Liam <liamxujia@outlook.com>
2021-06-02 16:34:05 +0800

    [SPM] Supported

---
+++ Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+  name: "React",
+  platforms: [.iOS(.v9)],
+  products: [
+    .library(name: "React", targets: ["react-ios"]),
+    .library(name: "DoubleConversion", targets: ["DoubleConversion"]),
+    .library(name: "glog", targets: ["glog"]),
+    .library(name: "yoga", targets: ["Yoga"])
+  ],
+  targets: [
+    .target(name: "React", path: "Example/", dependencies: ["DoubleConversion", "glog", "Yoga"])
+    .target(name: "DoubleConversion", path: "Example/")
+    .target(name: "glog", path: "Example/")
+    .target(name: "Yoga", path: "Example/")
+  ]
+)
